f64120425ed7dacdf5f944dd21a4fb0efe353abf
y_uuki <yuki.tsubo@gmail.com>
2012-05-30 16:08:28 +0900

    QuickSortもデフォルト引数対応

--- sorter.hpp
+++ sorter.hpp
@@ -7,6 +7,7 @@
 
 namespace mysorter {
 
+    // Selection Sort
     template <
         class RandomAccessIterator,
         class Predicate = std::less< typename std::iterator_traits<RandomAccessIterator>::value_type >
@@ -25,22 +26,11 @@ namespace mysorter {
 
     };
 
-    template<class RandomAccessIterator, class Predicate>
-    class QuickSort {
-        typedef typename std::iterator_traits<RandomAccessIterator>::value_type value_t;
+    // 
+    namespace _impl_quick_sort {
 
-        QuickSort(RandomAccessIterator const low, RandomAccessIterator const high, Predicate c) {
-            assert( low <= high );
-        }
-
-        public:
-        static void sort(RandomAccessIterator first, RandomAccessIterator last, Predicate pred)
-        {
-            qsort(first, last - 1, pred);
-        }
-
-        private:
-        static void qsort(typename std::vector<value_t>::iterator left, typename std::vector<value_t>::iterator right, Predicate pred)
+        template < class RandomAccessIterator, class Predicate >
+        static void qsort(RandomAccessIterator left, RandomAccessIterator right, Predicate pred)
         {
             if (left < right) {
                 auto pivot = left + std::distance(left, right) / 2;
@@ -57,25 +47,16 @@ namespace mysorter {
                 qsort(store + 1, right, pred);
             }
         }
-
-        // template<class IterT, class PredT>
-        // friend void quick_sort(IterT first, IterT last, PredT c);
-
-    };
-
-
-    template<class RandomAccessIterator, class Predicate>
-    static inline void quick_sort(RandomAccessIterator first, RandomAccessIterator last, Predicate pred)
-    {
-        typedef typename std::iterator_traits<RandomAccessIterator>::value_type value_t;
-        QuickSort<RandomAccessIterator,Predicate>::sort(first, last, pred);
     }
 
-    template<class RandomAccessIterator>
-    static inline void quick_sort(RandomAccessIterator first, RandomAccessIterator last)
+    template <
+        class RandomAccessIterator,
+        class Predicate = std::less< typename std::iterator_traits<RandomAccessIterator>::value_type >
+    >
+    static void quick_sort(RandomAccessIterator first, RandomAccessIterator last,
+            Predicate pred = std::less< typename std::iterator_traits<RandomAccessIterator>::value_type>())
     {
-        typedef typename std::iterator_traits<RandomAccessIterator>::value_type value_t;
-        QuickSort< RandomAccessIterator, std::less<value_t> >::sort(first, last, std::less<value_t>());
+        _impl_quick_sort::qsort<RandomAccessIterator, Predicate>(first, last - 1, pred);
     }
 
 }
